f79537806583ad2dd16364bd8b59fa864fb24c6c
Grzegorz Gomza <gomza.datascience@gmail.com>
2025-02-24 18:08:37 +0100

    Logging, Exception and Utils Module

---
+++ src/BreastCancerClassifier/utils/common.py
@@ -0,0 +1,172 @@
+# Standard library imports
+import os  # For operating system dependent functionality
+import json  # For working with JSON data
+import joblib  # For saving and loading Python objects (typically for machine learning models)
+import base64  # For encoding and decoding binary data to/from Base64 format
+from pathlib import Path  # For object-oriented filesystem paths
+from typing import Any  # For representing any type
+
+# Third-party library imports
+import yaml  # For processing YAML files
+from box.exceptions import BoxValueError  # Exception class for Box configuration errors
+from box import ConfigBox  # For using Box configurations 
+from ensure import ensure_annotations  # For enforcing type annotations at runtime
+
+# Local application/library imports
+from BreastCancerClassifier import logger  # Importing the logger from the BreastCancerClassifier module
+
+
+@ensure_annotations
+def read_yaml(yamlPath: Path) -> ConfigBox:
+    """
+    Reads a YAML file and returns its content as a ConfigBox object.
+
+    Args:
+        yamlPath (Path): The path to the YAML file.
+
+    Returns:
+        ConfigBox: A ConfigBox object containing the parsed YAML content.
+
+    Raises:
+        ValueError: If the YAML file is empty.
+        Exception: If there is an error reading the YAML file.
+    """
+
+    try:
+        with open(yamlPath, "r") as yamlFile:
+            content = yaml.safe_load(yamlFile)
+            logger.info(f"yaml file: {yamlPath} loaded successfully!")
+            return ConfigBox(content, yamlPath)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        print(f"Error reading yaml file: {yamlPath}\n Error: {e}")
+
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+
+    logger.info(f"json file saved at: {path}")
+
+@ensure_annotations
+def load_json(jsonPath: Path) -> ConfigBox:
+    """
+    Reads a JSON file and returns its content as a ConfigBox object.
+
+    Args:
+        jsonPath (Path): The path to the JSON file.
+
+    Returns:
+        ConfigBox: A ConfigBox object containing the parsed JSON content.
+
+    Raises:
+        ValueError: If the JSON file is empty.
+        Exception: If there is an error reading the JSON file.
+    """
+    try:
+        with open(jsonPath) as f:
+            content = json.load(f)
+
+        logger.info(f"json file loaded from: {jsonPath}")
+        return ConfigBox(content, jsonPath)
+    except BoxValueError:
+        raise ValueError("json file is empty")
+    except Exception as e:
+        print(f"Error reading json file: {jsonPath}\n Error: {e}")
+
+
+@ensure_annotations
+def save_bin_file(data: Any, binPath: Path):
+    """
+    Saves the given data to a binary file at the given path.
+
+    Args:
+        data (Any): The data to be saved to the binary file.
+        binPath (Path): The path where the binary file should be saved.
+    """
+    joblib.dump(data, binPath)
+    logger.info(f"bin file saved at: {binPath}")
+
+@ensure_annotations
+def load_bin_file(binPath: Path) -> Any:
+    """
+    Loads a binary file from the given path and returns its content.
+
+    Args:
+        binPath (Path): The path to the binary file to load.
+
+    Returns:
+        Any: The content of the binary file.
+
+    Raises:
+        FileNotFoundError: If the binary file does not exist.
+        Exception: If there is an error loading the binary file.
+    """
+    try:
+        data = joblib.load(binPath)
+        logger.info(f"bin file loaded from: {binPath}")
+        return data
+    except FileNotFoundError:
+        raise FileNotFoundError(f"bin file not found at: {binPath}")
+    except Exception as e:
+        print(f"Error loading bin file: {binPath}\n Error: {e}")
+
+
+@ensure_annotations
+def create_directories(dirPaths: list, verbose=True):
+    """
+    Creates one or more directories given a list of directory paths.
+
+    Args:
+        dirPaths (list): A list of directory paths to create.
+        verbose (bool, optional): If True, will log a message after creating each directory. Defaults to True.
+    """
+    for path in dirPaths:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"Created directory at: {path}")
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """
+    Returns the size of a file in KB.
+
+    Args:
+        path (Path): The path to the file.
+
+    Returns:
+        str: The size of the file in KB.
+    """
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"file size: ~ {size_in_kb} KB"
+
+def decode_image(imgString: str, fileName: str) -> None:
+    """
+    Decodes a base64 encoded image string and saves it to a file.
+
+    Args:
+        imgstring (str): The base64 encoded image string.
+        fileName (str): The file name to save the image as.
+
+    Returns:
+        None
+    """
+    imgdata = base64.b64decode(imgString)
+    with open(fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
+
+def encode_image_into_base64(imagePath: Path) -> str:
+    """
+    Encodes an image file into a base64 string.
+
+    Args:
+        imagePath (Path): The path to the image file to encode.
+
+    Returns:
+        base64: The base64 encoded string of the image file.
+    """
+    with open(imagePath, "rb") as f:
+        return base64.b64encode(f.read())
